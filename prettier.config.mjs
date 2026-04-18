750d21987c7ffdd318e89650043311581e43518e
devwqc <devwqc@gmail.com>
2026-04-18 14:03:04 +0900

    chore: prettier 패키지 추가

---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: [],
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
+
+export default config;
