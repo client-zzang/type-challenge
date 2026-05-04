518ef853fdc207331d425306824bb167d88241d2
minji <bmj05378@gmail.com>
2026-05-04 14:45:55 +0900

    feat: [Week2] 00898-easy-includes

---
+++ minji/00898-easy-includes.ts
@@ -0,0 +1,76 @@
+/*
+  898 - Includes
+  -------
+  by null (@kynefuk) #easy #array
+
+  ### Question
+
+  Implement the JavaScript `Array.includes` function in the type system. A type takes the two arguments. The output should be a boolean `true` or `false`.
+
+  For example:
+
+  ```ts
+  type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
+  ```
+
+  > View on GitHub: https://tsch.js.org/898
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rest,
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>
+  >,
+  Expect<
+    Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>
+  >,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/898/answer
+  > View solutions: https://tsch.js.org/898/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
+/*
+배운 점
+- 배열을 재귀적으로 순회할 때, 빈 배열일 때를 고려해야 한다.
+- T extends U: 정확한 일치를 비교하는 게 아님. 할당 가능성을 검사하는 것. "T가 U에 포함된다"라고 이해하기
+
+- boolean과 true/falase, readonly와 readonly가 아닌 객체 비교: 
+
+
+- 핵심 개념
+1. infer + 재귀
+2. 타입 동일성 비교하기
+3. 조건부 타입
+
+*/
