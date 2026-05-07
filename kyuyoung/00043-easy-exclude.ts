6dff93f8ddcccd1e38f08b4be56b3202de68ac1b
kyukyu <142164931+kyukyu-dev@users.noreply.github.com>
2026-05-07 13:02:42 +0900

    kyu: [Week2] 00043-easy-exclude

---
+++ kyuyoung/00043-easy-exclude.ts
@@ -0,0 +1,15 @@
+type MyExclude<T, U> = T extends U ? never : T
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
+  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>,
+]
+
+/*
+  배운점
+  - extends를 활용한 확장타입 판단
+*/
