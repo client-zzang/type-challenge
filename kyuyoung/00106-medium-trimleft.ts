08e3a064ff1dc7674188a4ad023c4bc0283f594c
kyukyu <142164931+kyukyu-dev@users.noreply.github.com>
2026-05-28 13:03:16 +0900

    kyu: [Week5] 00106-medium-trimleft

---
+++ kyuyoung/00106-medium-trimleft.ts
@@ -0,0 +1,20 @@
+type Space = ' ' | '\n' | '\t'
+type TrimLeft<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R> : S;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TrimLeft<'str'>, 'str'>>,
+  Expect<Equal<TrimLeft<' str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
+  Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
+  Expect<Equal<TrimLeft<''>, ''>>,
+  Expect<Equal<TrimLeft<' \n\t'>, ''>>,
+]
+
+/*
+  배운점
+  - Literal String type
+*/
