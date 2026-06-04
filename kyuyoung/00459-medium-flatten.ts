de3f85f19ee5f11dffd42c687aa16a81a586f8a2
kyukyu <142164931+kyukyu-dev@users.noreply.github.com>
2026-06-04 13:04:39 +0900

    kyu: [Week6] 00459-medium-flatten

---
+++ kyuyoung/00459-medium-flatten.ts
@@ -0,0 +1,21 @@
+type Flatten<T extends any[]> = T extends []
+  ? [] 
+  : T extends [infer F, ...infer R]
+    ? F extends any[]
+      ? [...Flatten<F>, ...Flatten<R>]
+      : [F, ...Flatten<R>]
+    : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Flatten<[]>, []>>,
+  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<Equal<Flatten<[{ foo: 'bar', 2: 10 }, 'foobar']>, [{ foo: 'bar', 2: 10 }, 'foobar']>>,
+]
+
+// @ts-expect-error
+type error = Flatten<'1'>
