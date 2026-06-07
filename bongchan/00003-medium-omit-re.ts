d58b89db0f1ad6cccefef257e54990fff95aa255
정봉찬 <devwqc@gmail.com>
2026-06-07 18:42:09 +0900

    bong(260607): [RE] 00003-medium-omit-re

---
+++ bongchan/00003-medium-omit-re.ts
@@ -0,0 +1,88 @@
+/*
+  3 - Omit
+  -------
+  by Anthony Fu (@antfu) #보통 #union #built-in
+
+  ### 질문
+
+  `T`에서 `K` 프로퍼티만 제거해 새로운 오브젝트 타입을 만드는 내장 제네릭 `Omit<T, K>`를 이를 사용하지 않고 구현하세요.
+
+  예시:
+
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+  type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+
+  const todo: TodoPreview = {
+    completed: false,
+  }
+  ```
+
+  > GitHub에서 보기: https://tsch.js.org/3/ko
+*/
+
+// 🚀 시작: 2026-06-07 18:32
+// ✅ 종료: 2026-06-07 18:35
+// 🥺 정답 확인 여부: X
+
+/*
+  🤔 접근
+    1. Mapped Types
+      - as를 활용해서 제네릭 K로 타입이 좁혀지면 never로 프로퍼티 생성 X
+
+  😆 배움
+    -
+
+*/
+
+/* _____________ 여기에 코드 입력 _____________ */
+
+type MyOmit<T, K> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
+  Expect<Equal<Expected3, MyOmit<Todo1, 'description' | 'completed'>>>,
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Todo1 {
+  readonly title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+  completed: boolean;
+}
+
+interface Expected2 {
+  title: string;
+}
+
+interface Expected3 {
+  readonly title: string;
+}
+
+/* _____________ 다음 단계 _____________ */
+/*
+  > 정답 공유하기: https://tsch.js.org/3/answer/ko
+  > 정답 보기: https://tsch.js.org/3/solutions
+  > 다른 문제들: https://tsch.js.org/ko
+*/
