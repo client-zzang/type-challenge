c4df20d04006aada74a978ad9865b65d1270fe42
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2026-07-10 13:07:40 +0900

    minji: [Week11] 02793-medium-mutable

---
+++ minji/02793-medium-mutable.ts
@@ -0,0 +1,73 @@
+/*
+  2793 - Mutable
+  -------
+  by jiangshan (@jiangshanmeta) #medium #readonly #object-keys
+
+  ### Question
+
+  Implement the generic ```Mutable<T>``` which makes all properties in ```T``` mutable (not readonly).
+
+  For example
+
+  ```typescript
+  interface Todo {
+    readonly title: string
+    readonly description: string
+    readonly completed: boolean
+  }
+
+  type MutableTodo = Mutable<Todo> // { title: string; description: string; completed: boolean; }
+
+  ```
+
+  > View on GitHub: https://tsch.js.org/2793
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Mutable<T extends Record<string, any>> = {-readonly [key in keyof T]: T[key]}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
+
+type List = [1, 2, 3]
+
+type cases = [
+  Expect<Equal<Mutable<Readonly<Todo1>>, Todo1>>,
+  Expect<Equal<Mutable<Readonly<List>>, List>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  Mutable<'string'>,
+  // @ts-expect-error
+  Mutable<0>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2793/answer
+  > View solutions: https://tsch.js.org/2793/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
+/*
+접근
+1. readonly를 mutable하게 바꾸는 방법?
+{[key in keyof T]: T[key]}는 key가 그대로 들어가서 readonly가 남아있게 됨
+
+2. -?: 사용하기?
+-> optional만 제거하는 거라 불가
+
+3. (정답) -readonly 사용하기
+
+*/
