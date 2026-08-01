7681d48b729ed5da1fadd50ef4129f7b35b509e9
정봉찬 <devwqc@gmail.com>
2026-08-01 13:55:26 +0900

    bong(260801): [Week15] 04179-medium-flip

---
+++ bongchan/04179-medium-flip.ts
@@ -0,0 +1,69 @@
+/*
+  4179 - Flip
+  -------
+  by Farhan Kathawala (@kathawala) #medium #object
+
+  ### Question
+
+  Implement the type of `just-flip-object`. Examples:
+
+  ```typescript
+  Flip<{ a: "x", b: "y", c: "z" }>; // {x: 'a', y: 'b', z: 'c'}
+  Flip<{ a: 1, b: 2, c: 3 }>; // {1: 'a', 2: 'b', 3: 'c'}
+  Flip<{ a: false, b: true }>; // {false: 'a', true: 'b'}
+  ```
+
+  No need to support nested objects and values which cannot be object keys such as arrays
+
+  > View on GitHub: https://tsch.js.org/4179
+*/
+
+// 🚀 시작: 2026-08-01 13:30
+// ✅ 종료: 2026-08-01 13:52
+// 🥺 정답 확인 여부: X
+
+/*
+  🤔 접근
+    1. Mapped Types
+
+      type MyPropertyKey = string | number | boolean;
+
+      type Flip<T> = {
+        [P in keyof T as `${T[P] & MyPropertyKey}`]: P;
+      };
+
+  😆 배움
+    - 다른 풀이
+
+      type Flip<T extends Record<string, string | number | boolean>> = {
+        [P in keyof T as `${T[P]}`]: P;
+      };
+
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyPropertyKey = string | number | boolean;
+
+type Flip<T> = {
+  [P in keyof T as `${T[P] & MyPropertyKey}`]: P;
+};
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect, NotEqual } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<{ a: 'pi' }, Flip<{ pi: 'a' }>>>,
+  Expect<NotEqual<{ b: 'pi' }, Flip<{ pi: 'a' }>>>,
+  Expect<Equal<{ 3.14: 'pi'; true: 'bool' }, Flip<{ pi: 3.14; bool: true }>>>,
+  Expect<
+    Equal<{ val2: 'prop2'; val: 'prop' }, Flip<{ prop: 'val'; prop2: 'val2' }>>
+  >,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4179/answer
+  > View solutions: https://tsch.js.org/4179/solutions
+  > More Challenges: https://tsch.js.org
+*/
