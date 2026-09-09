f4ece201940175741ad17016bce04c59ff8f21be
Minji <121149171+chosim-dvlpr@users.noreply.github.com>
2026-09-09 22:06:30 +0900

    minji: [Week20] 05821-medium-maptypes

---
+++ minji/05821-medium-maptypes.ts
@@ -0,0 +1,84 @@
+/*
+  5821 - MapTypes
+  -------
+  by Krzysztof "Wokay" Łokaj (@wokayme) #medium #map #object #utils
+
+  ### Question
+
+  Implement `MapTypes<T, R>` which will transform types in object T to different types defined by type R which has the following structure
+
+  ```ts
+  type StringToNumber = {
+    mapFrom: string; // value of key which value is string
+    mapTo: number; // will be transformed for number
+  }
+  ```
+
+  ## Examples:
+
+  ```ts
+  type StringToNumber = { mapFrom: string; mapTo: number;}
+  MapTypes<{iWillBeANumberOneDay: string}, StringToNumber> // gives { iWillBeANumberOneDay: number; }
+  ```
+
+  Be aware that user can provide a union of types:
+  ```ts
+  type StringToNumber = { mapFrom: string; mapTo: number;}
+  type StringToDate = { mapFrom: string; mapTo: Date;}
+  MapTypes<{iWillBeNumberOrDate: string}, StringToDate | StringToNumber> // gives { iWillBeNumberOrDate: number | Date; }
+  ```
+
+  If the type doesn't exist in our map, leave it as it was:
+  ```ts
+  type StringToNumber = { mapFrom: string; mapTo: number;}
+  MapTypes<{iWillBeANumberOneDay: string, iWillStayTheSame: Function}, StringToNumber> // // gives { iWillBeANumberOneDay: number, iWillStayTheSame: Function }
+  ```
+
+  > View on GitHub: https://tsch.js.org/5821
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Pick<V, R extends {mapFrom: any, mapTo: any}> =
+    R extends R ? (V extends R['mapFrom'] ? R['mapTo'] : never) : never
+type MapTypes<T extends Record<string, any>, R extends {mapFrom: any, mapTo: any}> = {[key in keyof T]: T[key] extends R['mapFrom'] ? Pick<T[key], R> : T[key]}
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MapTypes<{ stringToArray: string }, { mapFrom: string, mapTo: [] }>, { stringToArray: [] }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string }, { mapFrom: string, mapTo: number }>, { stringToNumber: number }>>,
+  Expect<Equal<MapTypes<{ stringToNumber: string, skipParsingMe: boolean }, { mapFrom: string, mapTo: number }>, { stringToNumber: number, skipParsingMe: boolean }>>,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date } | { mapFrom: string, mapTo: null }>, { date: null | Date }>>,
+  Expect<Equal<MapTypes<{ date: string }, { mapFrom: string, mapTo: Date | null }>, { date: null | Date }>>,
+  Expect<Equal<MapTypes<{ fields: Record<string, boolean> }, { mapFrom: Record<string, boolean>, mapTo: string[] }>, { fields: string[] }>>,
+  Expect<Equal<MapTypes<{ name: string }, { mapFrom: boolean, mapTo: never }>, { name: string }>>,
+  Expect<Equal<MapTypes<{ name: string, date: Date }, { mapFrom: string, mapTo: boolean } | { mapFrom: Date, mapTo: string }>, { name: boolean, date: string }>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/5821/answer
+  > View solutions: https://tsch.js.org/5821/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
+/*
+접근
+1. key, value로 순회하며 접근
+type MapTypes<T extends Record<string, any>, R extends {mapFrom: any, mapTo: any}> = {[key in keyof T]: T[key] extends R['mapFrom'] ? R['mapTo'] : T[key]}
+=> 마지막 케이스 미통과
+마지막 케이스가 이렇게 뜬다:
+{
+    name: string | boolean;
+    date: string | boolean;
+}
+R['mapFrom']이 string | Date이고
+R['mapTo']가 boolean | string이기 때문
+=> R 유니온 개수만큼 순회하기
+=> 결과도 유니온으로 나오는 문제 ... 튜플로 분해하기 ?
+
+=> 잘 모르겠다!
+
+*/
