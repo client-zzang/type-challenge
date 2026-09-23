9ede132d8dcb50aa72693aed95fe0037fa1f322b
정봉찬 <devwqc@gmail.com>
2026-09-23 21:50:41 +0900

    bong(260923): [RE] 03188-medium-tuple-to-nested-object-re

---
+++ bongchan/03188-medium-tuple-to-nested-object-re.ts
@@ -0,0 +1,72 @@
+/*
+  3188 - Tuple to Nested Object
+  -------
+  by jiangshan (@jiangshanmeta) #medium #object #tuple
+
+  ### Question
+
+  Given a tuple type ```T``` that only contains string type, and a type ```U```, build an object recursively.
+
+  ```typescript
+  type a = TupleToNestedObject<['a'], string> // {a: string}
+  type b = TupleToNestedObject<['a', 'b'], number> // {a: {b: number}}
+  type c = TupleToNestedObject<[], boolean> // boolean. if the tuple is empty, just return the U type
+  ```
+
+  > View on GitHub: https://tsch.js.org/3188
+*/
+
+// 🚀 시작: 2026-09-23 21:44
+// ✅ 종료: 2026-09-23 21:48
+// 🥺 정답 확인 여부: X
+
+/*
+  🤔 접근
+    1. Mapped Types value의 재귀
+
+      type TupleToNestedObject<T extends string[], U> = T extends [
+        infer F extends string,
+        ...infer R extends string[],
+      ]
+        ? {
+            [P in F]: TupleToNestedObject<R, U>;
+          }
+        : U;
+
+  😆 배움
+    - 
+
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type TupleToNestedObject<T extends string[], U> = T extends [
+  infer F extends string,
+  ...infer R extends string[],
+]
+  ? {
+      [P in F]: TupleToNestedObject<R, U>;
+    }
+  : U;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
+  Expect<
+    Equal<
+      TupleToNestedObject<['a', 'b', 'c'], boolean>,
+      { a: { b: { c: boolean } } }
+    >
+  >,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3188/answer
+  > View solutions: https://tsch.js.org/3188/solutions
+  > More Challenges: https://tsch.js.org
+*/
